d6075c8c79f388c224bf89d365b24244f0cfc696
yhechaichi <yaqine.hechaichi@gmail.com>
2016-09-27 19:09:47 +0200

    Test Commit

---
+++ src/Network.py
@@ -0,0 +1,10 @@
+class Network:
+    def __init__(self,neurons,inputs,outputs, expected_outputs, cost_neuron):
+        self.neurons = neurons
+        self.inputs = inputs
+        self.outputs = outputs
+        self.expected_outputs = expected_outputs
+        self.cost_neuron = cost_neuron
+
+    def 
+
---
+++ src/Neuron.py
@@ -0,0 +1,40 @@
+class Neuron:
+
+    def __init__(self,name,parents,init_function):
+        """Constructor used for initialising the neuron, declaring its name, its neuron parents and its weights"""
+        self.name = name
+        self.parents = parents
+        self.w = init_function(len(parents))
+        self.init_function = init_function
+        self.children, self.x, self.y, self.dJdx, self.acc_dJdw = None
+
+    def evaluate(self):
+        """Return the output of the neuron as a double"""
+
+    def get_gradient(self):
+        """Return the gradient of the neuron, based on its inputs and the associated weights"""
+
+    def descend_gradient(self,learning_rate,batch_size):
+        """Updates the weights related to the neuron"""
+
+    def add_child(self, enfant):
+        """Add a child to the neuron"""
+        self.children.append(enfant)
+
+    def reset_memoization(self):
+        """Reset memoization"""
+        self.x = None
+        self.y = None
+        self.dJdx = None
+
+    def reset_accumulator(self):
+        """Reset accumulator"""
+        self.acc_dJdw = None
+
+    def activation_function(self):
+        """Activation function for the neuron"""
+        raise NotImplementedError(self)
+
+    def gradient_activation_function(self):
+        """Calculate the derivative of the function"""
+        raise NotImplementedError(self)
