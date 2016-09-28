686186343741ae20873d05c3551d65f53e62f220
Pierre Vigier <pierre.vigier@ymail.com>
2016-09-28 17:09:25 +0200

    Add toy_datasets.py

---
+++ examples/toy_datasets.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def boolean_function_dataset(length, function, nb_inputs=2):
+	X = np.random.randint(0, 2, (length, nb_inputs))
+	Y = np.array([function(x) for x in X])
+	return X, Y
+
+def unit_square_function_dataset(length, function, nb_inputs=2):
+	X = np.random.rand(length, nb_inputs)
+	Y = np.array([function(x) for x in X])
+	return X, Y
+
+def or_dataset(length):
+	return boolean_function_dataset(length, lambda x: x[0] or x[1])
+
+def and_dataset(length):
+	return boolean_function_dataset(length, lambda x: x[0] and x[1])
+
+def xor_dataset(length):
+	return boolean_function_dataset(length, lambda x: (x[0] and not x[1]) or (not x[0] and x[1]))
+
+def plane_dataset(length, normal_vector=[1, -1]):
+	return unit_square_function_dataset(length, lambda x: (np.dot(x, normal_vector) >= 0))
+
+def disk_dataset(length):
+	return unit_square_function_dataset(length, lambda x: ((x[0]-0.5)**2 + (x[1]-0.5)**2) <= 0.0625)
+
+def augmented_dataset(X, map_function, input_length):
+	new_X = np.zeros((X.shape[0], input_length))
+	for i in range(X.shape[0]):
+		new_X[i,:] = map_function(X[i])
+	return new_X
